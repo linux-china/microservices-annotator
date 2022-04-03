3901d1c0f862fc877db91b2d4090a244acff0671
linux_china <libing.chen@gmail.com>
2022-04-02 17:29:42 -0700

    DataAccess

---
+++ src/main/java/org/mvnsearch/microservices/annotator/DataAccess.java
@@ -0,0 +1,22 @@
+package org.mvnsearch.microservices.annotator;
+
+import java.lang.annotation.*;
+
+/**
+ * Data access, such as NoSQL, cache and other data products.
+ * <p>
+ * If it's Database, please use {@link DatabaseAccess}
+ *
+ * @author linux_china
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface DataAccess {
+    /**
+     * data product name, such as redis, mongodb ...
+     *
+     * @return data product name
+     */
+    String value() default "";
+}
