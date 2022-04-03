efb3cdceb17a52381dada3b7ff6d5c8e57994d75
linux_china <libing.chen@gmail.com>
2022-04-02 17:29:31 -0700

    CloudStorage

---
+++ src/main/java/org/mvnsearch/microservices/annotator/CloudStorage.java
@@ -0,0 +1,20 @@
+package org.mvnsearch.microservices.annotator;
+
+import java.lang.annotation.*;
+
+/**
+ * cloud storage, such as S3, OSS, IPFS, block service ...
+ *
+ * @author linux_china
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface CloudStorage {
+    /**
+     * cloud storage name, such as s3, ipfs, oss
+     *
+     * @return storage system name
+     */
+    String value() default "";
+}
