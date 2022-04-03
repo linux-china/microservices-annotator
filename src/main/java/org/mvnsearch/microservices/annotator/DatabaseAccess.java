950c9c8f3e6b9471c26e83aa02849bbbbe4fc33b
linux_china <libing.chen@gmail.com>
2022-04-02 17:29:52 -0700

    DatabaseAccess

---
+++ src/main/java/org/mvnsearch/microservices/annotator/DatabaseAccess.java
@@ -0,0 +1,22 @@
+package org.mvnsearch.microservices.annotator;
+
+import java.lang.annotation.*;
+
+/**
+ * Database access, and focus on traditional relation database, such MySQL, PostgreSQL, Oracle ...
+ * <p>
+ * If it's NoSQL and other products, please consider to use {@link DataAccess}
+ *
+ * @author linux_china
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface DatabaseAccess {
+    /**
+     * data base name, such as mysql, postgres, oracle, sqlite ...
+     *
+     * @return database name
+     */
+    String value() default "";
+}
